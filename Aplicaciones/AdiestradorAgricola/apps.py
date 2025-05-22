a5ed2cee6fb30b646cd05e6f91babc55ddba8afc
Eddylucero <luceroeddy8@gmail.com>
2025-05-21 20:37:06 -0500

    Codificacion en Adiestrador en models, admin y app

--- Aplicaciones/AdiestradorAgricola/apps.py
+++ Aplicaciones/AdiestradorAgricola/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AdiestradoragricolaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AdiestradorAgricola'
+    name = 'Aplicaciones.AdiestradorAgricola'
--- Aplicaciones/AdiestradorAgricola/models.py
+++ Aplicaciones/AdiestradorAgricola/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class AdiestradorAgricola(models.Model):
+    nombre = models.CharField(max_length=255)
+    especialidad = models.CharField(max_length=100)
+    experiencia = models.IntegerField(help_text="Años de experiencia")
+
+    def __str__(self):
+        return f"{self.nombre} - {self.especialidad}"
