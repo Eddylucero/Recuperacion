657a9424f4ee41a61eecdbcd46350ebd7195c3ad
Eddylucero <luceroeddy8@gmail.com>
2025-05-21 20:39:22 -0500

    Codificacion en Perro detector en models, admin y app

--- Aplicaciones/PerroDetector/apps.py
+++ Aplicaciones/PerroDetector/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PerrodetectorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'PerroDetector'
+    name = 'Aplicaciones.PerroDetector'
--- Aplicaciones/PerroDetector/models.py
+++ Aplicaciones/PerroDetector/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class PerroDetector(models.Model):
+    nombre = models.CharField(max_length=255)
+    raza = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    estado = models.CharField(max_length=50, choices=[("En progreso", "En progreso"), ("Certificado", "Certificado")])
+
+    def __str__(self):
+        return f"{self.nombre} ({self.raza})"
