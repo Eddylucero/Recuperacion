3dd2e1131ef224b51c555fb2f40832e01adf42fe
Eddylucero <luceroeddy8@gmail.com>
2025-05-21 20:33:56 -0500

    Correccion en Settings de idioma y aplicaiones

--- proyecto50/settings.py
+++ proyecto50/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Aplicaciones.PerroDetector',
+    'Aplicaciones.Adiestradoragricola',
+    'Aplicaciones.SesionEntrenamiento',
 ]
 
 MIDDLEWARE = [
@@ -102,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ec-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Guayaquil'
 
 USE_I18N = True
 
