db967415789a8f64c0d90175b974175198b259d5
Eddylucero <luceroeddy8@gmail.com>
2025-05-22 00:25:36 -0500

    Creacion del Inicio

--- proyecto50/settings.py
+++ proyecto50/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'Aplicaciones.PerroDetector',
     'Aplicaciones.AdiestradorAgricola',
     'Aplicaciones.SesionEntrenamiento',
+    'Aplicaciones.Inicio',
 ]
 
 MIDDLEWARE = [
